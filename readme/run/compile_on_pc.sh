942da239ad412ba1ec02d2920a44ab4f3169264c
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-04-01 01:34:44 +0300

    Remove --Wc++11-extensions (unsupported)

--- readme/run/compile_on_pc.sh
+++ readme/run/compile_on_pc.sh
@@ -7,7 +7,7 @@ part2="run_animation_parser.cpp"
 part2res="run_animation_parser_pc"
 
 echo "Copmpiling... $part1 to $part1res"
-g++ --Wc++11-extensions $part1 -o $part1res
+g++ $part1 -o $part1res
 
 echo "Copmpiling... $part2 to $part2res"
-g++ --Wc++11-extensions $part2 -o $part2res
+g++ $part2 -o $part2res
